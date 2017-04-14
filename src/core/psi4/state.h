f511f9d3fe5f3dcfd31679fe5a6f89b9088d852a
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2017-04-14 11:52:01 +0200

    Starting headers

---
+++ src/core/psi4/state.h
@@ -0,0 +1,42 @@
+/* State representation */
+#ifndef PSI4_STATE_H
+#define PSI4_STATE_H
+
+#include <vector>
+
+namespace psi4 {
+	/** Set implementation ; internal structure
+	 */
+	abstract public class SetImpl {
+
+	}
+
+	/** One point in the state space 
+	 */ 
+	public class Point : vector<int>, SetImpl {
+		
+	}
+
+	/** Subset of the state space with a shape of hyperrectangle
+	 */ 
+	public class HyperRectangle : SetImpl {
+		private Point inf, sup;
+		
+	}
+
+	/** Union of subsets of the state space 
+	 */ 
+	public class Union : SetImpl {
+		private List<SetImpl> list;
+	}
+
+	
+	
+	/** Class that is a bridge to the actual set implementation
+	 */
+	public class Set {
+		private SetImpl realset;
+	}
+}
+
+#endif
---
+++ src/core/psi4/transition.h
@@ -0,0 +1,54 @@
+/* Transition representation */
+#ifndef PSI4_TRANSITION_H
+#define PSI4_TRANSITION_H
+
+#include <psi4/state.h>
+
+namespace psi4 {
+
+	abstract public class Transition {
+		
+	public:
+		int apply(Set s, Event ev)
+		{
+			SetImpl &src=s.realset;
+			SetImpl &res=apply(src, ev)
+			s.realset = res;
+		}
+		/** Apply transition to a point 
+		 */
+		SetImpl& apply(Point &p, Event)
+		{
+			foreach (int ind: p) {
+				p[ind] = max(0,p[ind] -1);
+			}
+			return p;
+		}
+		/** Apply transition to a set of states with
+		 * hyperrectangle structure
+		 */
+		SetImpl apply(HyperRectangle h, Event) {
+			// Split
+			Union myUnion;
+			myUnion = new Union();
+			// En place
+			myTransition(h);
+			myUnion.add(h);
+			foreach (Point p : findPointsOutside(h)) {
+				myUnion.add(p);
+			}
+			return myUnion;
+
+
+			// Classique
+			return myTransition(h);
+		}
+			
+		SetImpl apply(Union, Event);
+			
+	}
+
+
+}
+
+#endif
