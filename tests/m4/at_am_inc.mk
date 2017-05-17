1f689dc147f1ebd05e3a5b25a074839061494392
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2017-05-17 11:35:14 +0200

    [tests] basic automake support for autotest
    This requires GNU Make

---
+++ tests/m4/at_am_inc.mk
@@ -0,0 +1,24 @@
+#_at_m4_join: $1: sep, $2: first $3: second
+_at_m4_join = $(if $(and $2,$3),$2$1$3,$2$3)
+
+define _at_m4_gen # $1: AM_name  $2: filename
+$1_TESTSUITEFLAGS ?= $$(AM_TESTSUITEFLAGS) $$(TESTSUITEFLAGS)
+$1_ATPATH ?= $$(call _at_m4_join,:,$$(AM_ATPATH),$$(AUTOTEST_PATH))
+$1_MAIN ?= $2.at
+$$(srcdir)/$1: $$($1_MAIN) $$($1_DEPENDS) $$(srcdir)/package.m4
+	$$(AUTOTEST) -I '$$(srcdir)' -o $$@.tmp $$@.at
+	mv $$@.tmp $$@
+.PHONY: check-$1 clean-$1 clean-local
+check-$1: $$(srcdir)/$2 atconfig atlocal
+	$$(SHELL) '$$<' \
+		$$(if $$($1_ATPATH),AUTOTEST_PATH='$$($1_ATPATH)') \
+		$$(testsuite__installed_ATFLAGS) $$(ATFLAGS)
+clean-$1:
+	test ! -f '$$(srcdir)/$2' || \
+	$$(SHELL) '$$(srcdir)/$2' --clean
+clean-local: clean-$1
+EXTRA_DIST += $$($1_MAIN) $$($1_DEPENDS)
+endef
+
+$(foreach _at_am_testsuite,$(AT_TESTSUITES),\
+$(eval $(call _at_m4_gen,$(shell echo $(_at_am_testsuite) | $(SED) -e 's/[^[:alnum:]]/_/g'),$(strip $(_at_am_testsuite)))))
--- tests/m4/marto-autotest.m4
+++
@@ -1,6 +0,0 @@
-
-m4_include([m4/at_env.m4])
-m4_include([check-headers/check-headers.m4])
-m4_include([m4/at_named-test-groups.m4])
-
-
