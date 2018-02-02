8d5d9c077f59d788bba44e60e365ac7e27736a68
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2018-02-02 21:58:15 +0100

    Missing file required for compilation
    To be improved...

---
+++ src/core/random.cpp
@@ -0,0 +1,19 @@
+#include <marto/random.h>
+
+namespace marto {
+
+double Random::next() {
+    /* FIXME */
+    /* but garantee true random with a roll dice */
+    return 4;
+};
+
+/*  internal generic-type uniform generator */
+RandomUniformInterval::RandomUniformInterval(double _inf, double _sup): inf(_inf), sup(_sup) {
+};
+
+double RandomUniformInterval::next() {
+    return (inf + (sup - inf) * Random::next());
+};
+
+}
