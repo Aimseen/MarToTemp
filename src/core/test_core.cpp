e4a7b7377df06007a92e692697378dc41f394705
Pere Labat <guillaume.huard@imag.fr>
2018-02-09 10:42:34 +0000

    Ajout d4une transition bidon, compile

--- src/core/test_core.cpp
+++ src/core/test_core.cpp
@@ -3,13 +3,23 @@
 
 using namespace marto;
 
+class TransitionBidon:public Transition {
+    Point *apply(Point * p, Event * ev) {
+        for (int i = 0; i < 3; i++)
+            p->at(i)++;
+    }
+};
+
 // Do test stuff
 int main() {
+    // Fill the hardcoded transition names
+    Global::getConfig()->setTransition("TransitionBidon", new TransitionBidon());
+
     Point *p = new Point();
     p->resize(3);
     for (int i = 0; i < 3; i++)
         p->at(i) = i + 1;
-    EventType *et = new EventType("My super event", 42.0, "JSQ2", new FormalParameters());
+    EventType *et = new EventType("My super event", 42.0, "TransitionBidon", new FormalParameters());
     Event *e = new Event(et);
     
     std::cout << p << std::endl;
