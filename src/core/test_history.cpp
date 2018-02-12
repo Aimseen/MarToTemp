cc0916d8b12c708a1f4a19132dd7ae5a7f3b2c13
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2018-02-12 16:54:31 +0100

    Use googletest for the test_history

--- src/core/test_history.cpp
+++ src/core/test_history.cpp
@@ -1,9 +1,11 @@
 #include <marto.h>
-#include <iostream>
+#include "gtest/gtest.h"
+
+namespace {
 
 using namespace marto;
 
-class TransitionBidon:public Transition {
+class TransitionTest:public Transition {
     Point *apply(Point * p, __attribute__((unused)) Event * ev) {
         for (int i = 0; i < 3; i++)
             p->at(i)++;
@@ -17,20 +19,88 @@ class TransitionBidon:public Transition {
     }
 };
 
-// Do test stuff
-int main() {
+// The fixture for testing class Foo.
+class EventsHistoryBaseTest : public ::testing::Test {
+protected:
+    EventsHistoryBaseTest() {
+        c=Global::getConfig();
+        // ensure TransitionTest exists. Can return NULL if already registered.
+        c->registerTransition("TransitionTest", new TransitionTest());
+        et = new EventType(c, "My super event", 42.0, "TransitionTest", new FormalParameters());
+        e = new Event(et);
+        h = new EventsHistory(c);
+    }
+
+    virtual ~EventsHistoryBaseTest() {
+        // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    virtual void SetUp() {
+        ASSERT_TRUE(c);
+        ASSERT_TRUE(et);
+        ASSERT_TRUE(e);
+        ASSERT_TRUE(h);
+    }
+
+    virtual void TearDown() {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+    }
+
+    Configuration *c;
+    EventType *et;
+    Event *e;
+    EventsHistory *h;
+};
+
+TEST(Configuration, RegisterTransitionTwice) {
     auto c=Global::getConfig();
-    c->setTransition("TransitionBidon", new TransitionBidon());
-    auto h=new EventsHistory(c);
-    EventType *et = new EventType(c, "My super event", 42.0, "TransitionBidon", new FormalParameters());
-    Event *e = new Event(et);
+    Transition* tr=new TransitionTest();
+    ASSERT_EQ(tr, c->registerTransition("TransitionTestDupName", tr));
+    ASSERT_EQ(tr, c->registerTransition("TransitionTestDupName", tr));
+}
+
+TEST(Configuration, RegisterEventTypeWithUnknownTransition) {
+    auto c=Global::getConfig();
+    // ensure TransitionTest exists. Can return NULL if already registered.
+    c->registerTransition("TransitionTest", new TransitionTest());
+    try {
+        new EventType(c, "My super event", 42.0, "UnknownTransitionForTest", new FormalParameters());
+        ASSERT_FALSE(c->getTransition0("UnknownTransitionForTest"))
+            << "Transition 'UnknownTransitionForTest' should not exists";
+        FAIL() << "EventType successfully created with an unknown transition";
+    } catch (const UnknownTransition &e) {
+        SUCCEED();
+    }
+}
+
+TEST(Configuration, RegisterEventTypeTwice) {
+    auto c=Global::getConfig();
+    // ensure TransitionTest exists. Can return NULL if already registered.
+    c->registerTransition("TransitionTest", new TransitionTest());
+    ASSERT_TRUE(new EventType(c, "My super event", 42.0, "TransitionTest", new FormalParameters()));
+    ASSERT_TRUE(new EventType(c, "My super event", 42.0, "TransitionTest", new FormalParameters()));
+}
+
+// Tests that writing a undefined event correctly fails
+TEST_F(EventsHistoryBaseTest, writeUndefinedEvent) {
     auto it=h->iterator();
-    auto res=it->storeNextEvent(e);
-    assert(res == EVENT_STORE_UNDEFINED_ERROR);
+    ASSERT_TRUE(it);
+    ASSERT_EQ(EVENT_STORE_UNDEFINED_ERROR, it->storeNextEvent(e));
+}
+
+// Tests that it is possible to write 10 events
+TEST_F(EventsHistoryBaseTest, writeEvents) {
+    auto it=h->iterator();
+    ASSERT_TRUE(it);
     e->generate();
     for (int i=0; i<10; i++) {
-        res=it->storeNextEvent(e);
-        assert(res == EVENT_STORED);
+        ASSERT_EQ(EVENT_STORED, it->storeNextEvent(e));
     }
-    return 0;
 }
+
+}  // namespace
+
